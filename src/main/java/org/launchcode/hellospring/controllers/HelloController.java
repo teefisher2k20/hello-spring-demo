ba06af2cacd6079308d5776a03436a45212ffae1
Terrance Fisher <teefisher314@gmail.com>
2021-03-05 19:34:18 -0600

    Controller Exercises

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -27,8 +27,9 @@ public class HelloController {
 
     // Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, @RequestParam String language) {
+        String properGreeting = HelloController.createMessage(name, language);
+        return "<h3 style='color:blue;'>" + properGreeting + "</h3>";
     }
 
     // Handles requests of the form /hello/LaunchCode
@@ -44,10 +45,35 @@ public class HelloController {
                 "<body>" +
                 "<form action = 'hello' method = 'post'>" + // submit a request to /hello
                 "<input type = 'text' name = 'name' >" +
+                "<select name='language'>" +
+                    "<option value='English'>English</option>" +
+                    "<option value='Spanish'>Spanish</option>" +
+                    "<option value='German'>German</option>" +
+                    "<option value='French'>French</option>" +
+                    "<option value='Italian'>Italian</option>" +
+                "</select>" +
                 "<input type = 'submit' value = 'Greet Me!' >" +
                 "</form>" +
                 "</body>" +
                 "</html>";
     }
 
+    public static String createMessage(String name, String language) {
+
+        switch (language) {
+
+            case "Spanish":
+                return "Hola, " + name;
+            case "German":
+                return "Hallo, " + name;
+            case "French":
+                return "Bonjour, " + name;
+            case "Italian":
+                return "Ciao, " + name;
+            case "English":
+                default:
+                return "Hello, " + name;
+
+        }
+    }
 }
